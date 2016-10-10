12502e20b15546fd5502618d94fa186cc6a84265
Engineered Truth <Tangoforthemango@gmail.com>
2016-10-10 13:17:12 +0800

    deodorant evaporator

---
+++ deodorant-evaporator.js
@@ -0,0 +1,14 @@
+function evaporator(content, evap_per_day, threshold){
+
+thresholdAmount = content*(threshold/100);
+let n = 0;
+
+while(content > thresholdAmount){
+	content = content-content*(evap_per_day/100);
+	n++;
+}
+
+  return n;
+}
+
+Test.assertEquals(evaporator(10,10,10),22);
